eaba05fd4bedf8226a281da3a2e9e50ea8fa5a9a
Douglas Baumgratz <douglas.baumgratz@gmail.com>
2021-05-06 00:58:00 -0300

    Adiciona paginação

--- backend/src/controllers/IncidentController.js
+++ backend/src/controllers/IncidentController.js
@@ -2,7 +2,25 @@ const connection = require("../database/connection");
 
 module.exports = {
   async index(req, res) {
-    const incidents = await connection("incidents").select("*");
+    const { page = 1 } = req.query;
+
+    const [count] = await connection("incidents").count();
+    res.header("X-Total-Count", count["count(*)"]);
+
+    const incidents = await connection("incidents")
+    //TODO: ong.id ou ong_id
+      .join("ongs", "ong_id", "=", "incidents.ong_id")
+      .limit(5)
+      .offset((page - 1) * 5)
+      .select([
+        "incidents.*",
+        "ongs.name",
+        "ongs.email",
+        "ongs.whatsapp",
+        "ongs.city",
+        "ongs.uf",
+      ]);
+
     return res.json(incidents);
   },
 
